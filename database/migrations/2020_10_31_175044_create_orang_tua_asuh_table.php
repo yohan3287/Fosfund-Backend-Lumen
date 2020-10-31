19036c806bd50c1a1265b3ce6c161d1c8d6c98ff
yohan <yohan@binus.ac.id>
2020-11-01 00:54:21 +0700

    buat migration file create_orang_tua_asuh_table, revisi ';' di migration file create_admin_table

--- database/migrations/2020_10_31_175044_create_orang_tua_asuh_table.php
+++ database/migrations/2020_10_31_175044_create_orang_tua_asuh_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminTable extends Migration
+class CreateOrangTuaAsuhTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateAdminTable extends Migration
      */
     public function up()
     {
-        Schema::create('admin', function (Blueprint $table) {
+        Schema::create('orang_tua_asuh', function (Blueprint $table) {
             $table->id();
             $table
                 ->foreignId('user_id')
                 ->constrained('user')
                 ->onDelete('cascade');
             $table->string('nama');
-            $table->string('jabatan');
+            $table->string('telepon');
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ class CreateAdminTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('orang_tua_asuh');
     }
 }
