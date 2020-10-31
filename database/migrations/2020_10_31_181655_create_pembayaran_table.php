e9b4b13baabcccd8b736d09b0aab1fd4cb369126
yohan <yohan@binus.ac.id>
2020-11-01 01:21:08 +0700

    buat migration file create_pembayaran_table

---
+++ database/migrations/2020_10_31_181655_create_pembayaran_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePembayaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->id();
+            $table
+                ->foreignId('orang_tua_asuh_id')
+                ->constrained('orang_tua_asuh')
+                ->onDelete('cascade');
+            $table->string('bukti_bayar_img_path')
+            $table
+                ->foreignId('admin_verifier_id')
+                ->constrained('admin')
+                ->onDelete('cascade');
+            $table->dateTime('waktu_admin_verif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembayaran');
+    }
+}
