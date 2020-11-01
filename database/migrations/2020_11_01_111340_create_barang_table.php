50ea8a984ad8bbd6f666e92ed485728775e46def
yohan <yohan@binus.ac.id>
2020-11-01 18:16:58 +0700

    buat migration file create_barang_table

---
+++ database/migrations/2020_11_01_111340_create_barang_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table
+                ->foreignId('pembelanjaan_donasi_id')
+                ->constrained('pembelanjaan_donasi')
+                ->onDelete('cascade');
+            $table->string('nama');
+            $table->integer('jumlah');
+            $table->integer('harga_satuan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
