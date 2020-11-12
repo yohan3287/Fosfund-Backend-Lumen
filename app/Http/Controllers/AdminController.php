a066043eebd3a9d7953eccfac48ee427419b61c7
yohan <yohan@binus.ac.id>
2020-11-12 18:00:28 +0700

    tambah validasi di AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -55,29 +55,32 @@ class AdminController extends Controller
     public function verifPembayaran($order_id) {
         $adminID = $this->getAdminID();
 
-        DB::beginTransaction();
-        $result = DB::update('
-            UPDATE `order`
-            SET `order`.`admin_verifier_pembayaran_id` = ?,
-                `order`.`waktu_verif_pembayaran` = CURRENT_TIMESTAMP
-            WHERE `id` = ?
-                AND `order`.`bukti_bayar_doc_path` != NULL
-                AND `order`.`admin_verifier_pembayaran_id` = NULL;
-        ', [$adminID, $order_id]);
+        if ($adminID) {
+            DB::beginTransaction();
+            $result = DB::update('
+                UPDATE `order`
+                SET `order`.`admin_verifier_pembayaran_id` = ?,
+                    `order`.`waktu_verif_pembayaran` = CURRENT_TIMESTAMP
+                WHERE `id` = ?
+                    AND `order`.`bukti_bayar_doc_path` != NULL
+                    AND `order`.`admin_verifier_pembayaran_id` = NULL;
+            ', [$adminID, $order_id]);
 
-        if ($result) {
-            DB::commit();
-            return response()->json([
-                'success' => true,
-                'data' => $result
-            ],200);
-        } else {
-            DB::rollBack();
-            return response()->json([
-                'success' => false,
-                'data' => ''
-            ],400);
+            if ($result) {
+                DB::commit();
+                return response()->json([
+                    'success' => true,
+                    'data' => $result
+                ],200);
+            } else {
+                DB::rollBack();
+            }
         }
+
+        return response()->json([
+            'success' => false,
+            'data' => ''
+        ],400);
     }
 
     public function getUnverifiedSekolah() {
@@ -105,28 +108,31 @@ class AdminController extends Controller
     public function verifSekolah($sekolah_id) {
         $adminID = $this->getAdminID();
 
-        DB::beginTransaction();
-        $result = DB::update('
-            UPDATE `sekolah`
-            SET `sekolah`.`admin_verifier_id` = ?,
-                `sekolah`.`waktu_verif` = CURRENT_TIMESTAMP
-            WHERE `sekolah`.`id` = ?
-                AND `sekolah`.`admin_verifier_id` = NULL;
-        ', [$adminID, $sekolah_id]);
+        if ($adminID) {
+            DB::beginTransaction();
+            $result = DB::update('
+                UPDATE `sekolah`
+                SET `sekolah`.`admin_verifier_id` = ?,
+                    `sekolah`.`waktu_verif` = CURRENT_TIMESTAMP
+                WHERE `sekolah`.`id` = ?
+                    AND `sekolah`.`admin_verifier_id` = NULL;
+            ', [$adminID, $sekolah_id]);
 
-        if ($result) {
-            DB::commit();
-            return response()->json([
-                'success' => true,
-                'data' => $result
-            ],200);
-        } else {
-            DB::rollBack();
-            return response()->json([
-                'success' => false,
-                'data' => ''
-            ],400);
+            if ($result) {
+                DB::commit();
+                return response()->json([
+                    'success' => true,
+                    'data' => $result
+                ],200);
+            } else {
+                DB::rollBack();
+            }
         }
+
+        return response()->json([
+            'success' => false,
+            'data' => ''
+        ],400);
     }
 
     public function getUnverifiedPengajuanAA() {
@@ -155,29 +161,32 @@ class AdminController extends Controller
     public function verifPengajuanAA($pengajuan_aa_id) {
         $adminID = $this->getAdminID();
 
-        DB::beginTransaction();
-        $result = DB::update('
-            UPDATE `pengajuan_anak_asuh`
-            SET  `pengajuan_anak_asuh`.`admin_verifier_id` = ?,
-                `pengajuan_anak_asuh`.`waktu_verif` = CURRENT_TIMESTAMP
-            WHERE `pengajuan_anak_asuh`.`id` = ?
-                AND `pengajuan_anak_asuh`.`admin_verifier_id` = NULL
-                AND `pengajuan_anak_asuh`.`form_doc_path` != NULL;
-        ', [$adminID, $pengajuan_aa_id]);
+        if ($adminID) {
+            DB::beginTransaction();
+            $result = DB::update('
+                UPDATE `pengajuan_anak_asuh`
+                SET  `pengajuan_anak_asuh`.`admin_verifier_id` = ?,
+                    `pengajuan_anak_asuh`.`waktu_verif` = CURRENT_TIMESTAMP
+                WHERE `pengajuan_anak_asuh`.`id` = ?
+                    AND `pengajuan_anak_asuh`.`admin_verifier_id` = NULL
+                    AND `pengajuan_anak_asuh`.`form_doc_path` != NULL;
+            ', [$adminID, $pengajuan_aa_id]);
 
-        if ($result) {
-            DB::commit();
-            return response()->json([
-                'success' => true,
-                'data' => $result
-            ],200);
-        } else {
-            DB::rollBack();
-            return response()->json([
-                'success' => false,
-                'data' => ''
-            ],400);
+            if ($result) {
+                DB::commit();
+                return response()->json([
+                    'success' => true,
+                    'data' => $result
+                ],200);
+            } else {
+                DB::rollBack();
+            }
         }
+
+        return response()->json([
+            'success' => false,
+            'data' => ''
+        ],400);
     }
 
     public function getMatchedData() {
